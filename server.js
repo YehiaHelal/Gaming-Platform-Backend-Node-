d94995e29f8e19d2be2acb8b5fb1841a97b4ffe2
Yehia Helal <mangetsuhozuki287@yahoo.com>
2023-06-15 08:21:14 +0300

    9

--- server.js
+++ server.js
@@ -43,10 +43,11 @@ app.use((req, res, next) => {
 
 app.use(cookieParser()); // To parse the incoming cookies
 app.use(
-  cors({
-    credentials: true,
-    origin: "https://gaming-platform-frontend-next.vercel.app",
-  })
+  cors()
+  //   {
+  //   credentials: true,
+  //   origin: "https://gaming-platform-frontend-next.vercel.app",
+  // }
 );
 
 // app.use(cors());
